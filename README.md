44af752dba5184329d7af9095ea46590691e2796
brendan nyambati <brendan.nyambati@gmail.com>
2019-04-01 15:12:01 -0500

    login and tabbed pages

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # GLR Transport, Inc. App
 This is an internal application for use by GLR Transport truck drivers and managers.
+
+
+LOGIN
+username = jimmy
+password = pass
---
+++ glrtransport/Driver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace glrtransport
+{
+    public class Driver
+    {
+
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String FreightBillNumber { get; set; }
+
+        public Driver()
+        {
+        }
+    }
+}
